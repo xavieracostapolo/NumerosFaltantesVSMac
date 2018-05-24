2ece99b61c49d40b81a407cce2f35f12d0752f5a
Xavier Acosta <xavieracosta@ingenieros.com>
2018-05-23 19:57:39 -0500

    first commit

---
+++ BuscaNumeros/BusquedaNumero.cs
@@ -0,0 +1,49 @@
+﻿/*
+    Ing. Xavier Acosta Polo
+    23 de mayo de 2018
+*/
+
+namespace BuscaNumeros
+{
+    using System;
+    using System.Collections.Generic;
+
+
+    /// <summary>
+    /// Clase de Busqueda numero.
+    /// </summary>
+    public static class BusquedaNumero
+    {
+        /// <summary>
+        /// Gets the busqueda numero.
+        /// </summary>
+        /// <returns>Array del resultado en la busqueda de los numero.</returns>
+        /// <param name="arr">Arr. Primer array con los numeros perdidos</param>
+        /// <param name="brr">Brr. Segundo array permutacion de la primera</param>
+        public static int[] GetBusquedaNumero(int[] arr, int[] brr)
+        {
+            //Array para agregar el resultado que se desea obtener
+            List<int> result = new List<int>();
+
+            //Recorrer la segunda lista
+            int ib = 0;
+
+            //Recorrer la primera lista
+            for (int ia = 0; ia < arr.Length; ia++){
+                if(arr[ia] != brr[ib]){
+                    if(!result.Contains(brr[ib])){
+                        result.Add(brr[ib]);
+                    }
+
+                    ia -= 1;
+                }
+
+                ib += 1;
+            }
+
+            int[] arrayResult = result.ToArray();
+            Array.Sort(arrayResult);
+            return arrayResult;
+        }
+    }
+}
---
+++ BuscaNumeros/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+    Ing. Xavier Acosta Polo
+    23 de mayo de 2018
+*/
+
+namespace BuscaNumeros
+{
+    using System;
+
+    /// <summary>
+    /// Main class.
+    /// </summary>
+    class MainClass
+    {
+
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name="args">The command-line arguments.</param>
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            int m = Convert.ToInt32(Console.ReadLine());
+            int[] brr = Array.ConvertAll(Console.ReadLine().Split(' '), brrTemp => Convert.ToInt32(brrTemp));
+
+            //Ordenar segunda lista.
+            Array.Sort(brr);
+
+            //Ordenar el primer array
+            Array.Sort(arr);
+
+            //Configuracion Constraints
+            if (!(n <= m)) {
+                
+                Console.WriteLine("La primera lista debe ser menor que la segunda.");   
+
+            } else if (!((n >= 1) && (m <= 200000))) {
+                
+                Console.WriteLine("La primera lista debe ser mayor a 1 y la segunda menor a 2000000");
+
+            } else if (!((arr[0] - arr[n - 1] <= 100) && (brr[0] - brr[m - 1] <= 100)))
+            {
+                
+                Console.WriteLine("La diferencia entre el maximo y minimo valor de las listas debe ser 100");
+
+            } else {
+                
+                int[] result = BusquedaNumero.GetBusquedaNumero(arr, brr);
+                Console.WriteLine(string.Join(" ", result));
+
+            }
+        }
+    }
+}
